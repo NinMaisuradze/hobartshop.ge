32926d5b4d2d9f5491102081b2c78fd059928ec0
EleneNinoMaisuradze <ninmaisuradze20@gmail.com>
2025-11-10 11:24:44 +0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import "./Hero.css";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next"; 
-import s1 from "../components/images/s1.jpg";
-import s2 from "../components/images/s2.jpg";
-import s3 from "../components/images/s3.jpg";
-import s4 from "../components/images/s4.jpg";
-import s5 from "../components/images/S5.jpg"; 
+import s1 from "./images/s1.jpg";
+import s2 from "./images/s2.jpg";
+import s3 from "./images/s3.jpg";
+import s4 from "./images/s4.jpg";
+import s5 from "./images/s5.jpg";
+
 
 export default function Hero() {
   const { t } = useTranslation(); 
